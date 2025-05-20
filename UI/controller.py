2de8c42f5722510984892e5a96abccd608b33b2f
Davide Buoso <daviibuoso@gmail.com>
2025-05-20 10:43:39 +0200

    initial commit

---
+++ UI/controller.py
@@ -0,0 +1,28 @@
+import flet as ft
+
+
+class Controller:
+    def __init__(self, view, model):
+        # the view, with the graphical elements of the UI
+        self._view = view
+        # the model, which implements the logic of the program and holds the data
+        self._model = model
+
+        self._listYear = []
+        self._listCountry = []
+
+    def fillDD(self):
+        pass
+
+
+    def handle_graph(self, e):
+        pass
+
+
+
+    def handle_volume(self, e):
+        pass
+
+
+    def handle_path(self, e):
+        pass
